52ff5dc8c5f094b09a08e16d6d769b66600dc6b2
kirathenics <bka32129@gmail.com>
2023-11-23 19:33:01 +0300

    Добавьте файлы проекта.

---
+++ M_SPLab3Main/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() 
+{
+    setlocale(0, "");
+
+    std::string libName = "StringFunctionsDll.dll";
+    HMODULE hDLL = LoadLibrary((LPCSTR)"StringFunctionsDll.dll");  // загрузить библиотеку
+
+    if (hDLL == NULL)
+    {
+        std::cout << "Не удалось загрузить библиотеку DLL!" << std::endl;
+        exit(-1);
+    }
+
+    // получить указатели на экспортируемые функции из библиотеки
+    int (*countChar)(const char*, char) = (int (*)(const char*, char))GetProcAddress(hDLL, "countChar");
+    char (*mostFrequentChar)(const char*) = (char (*)(const char*))GetProcAddress(hDLL, "mostFrequentChar");
+
+    if ((!countChar) && (!mostFrequentChar))
+    {
+        std::cout << "Не удалось получить указатели на функции библиотеки DLL!" << std::endl;
+        exit(-1);
+    }
+
+    std::string text = "Пример текста для подсчета символов";
+    char target = 'т';
+
+    int charCount = countChar(text.c_str(), target);
+    std::cout << "Символ '" << target << "' встречается " << charCount << " раз(а)." << std::endl;
+
+    char mostFrequent = mostFrequentChar(text.c_str());
+    std::cout << "Cамый часто встречающийся символ '" << mostFrequent << "'." << std::endl;
+
+    FreeLibrary(hDLL);  // выгрузить библиотеку
+
+    return 0;
+}
